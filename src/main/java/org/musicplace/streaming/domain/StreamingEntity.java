eaf47e108e08f4b5c0c8421d9ae5235982573368
jucheolkang <jucheol9877@naver.com>
2024-05-05 15:17:15 +0900

    feat : StreamigEntity 1:M 관계 추가

--- src/main/java/org/musicplace/streaming/domain/StreamingEntity.java
+++ src/main/java/org/musicplace/streaming/domain/StreamingEntity.java
@@ -1,16 +1,23 @@
 package org.musicplace.streaming.domain;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.Comment;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -38,6 +45,19 @@ public class StreamingEntity {
     @Column(name = "INTRODUCE", nullable = true)
     @Comment("스트리밍 소개글")
     private String introduce;
+
+    @JsonManagedReference
+    @OneToMany(mappedBy = "StreamingEntity", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private List<StreamingChatEntity> chatEntities = new ArrayList<>();
+
+    @JsonManagedReference
+    @OneToMany(mappedBy = "StreamingEntity", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private List<StreamingMemberEntity> memberEntities = new ArrayList<>();
+
+    @JsonManagedReference
+    @OneToMany(mappedBy = "StreamingEntity", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private List<StreamingMusicEntity> musicEntities = new ArrayList<>();
+
 
     @Builder
     public StreamingEntity(String streamerNickname, String broadcastingTitle, String introduce) {
