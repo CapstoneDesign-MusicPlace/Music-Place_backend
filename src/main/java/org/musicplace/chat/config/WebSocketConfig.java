a12b24bdc47627c2f70bb5c44eb780644c523afb
boeun02 <qkrqhrud1118@naver.com>
2024-09-13 17:10:18 +0900

    feat: 웹소켓 작업 중

---
+++ src/main/java/org/musicplace/chat/config/WebSocketConfig.java
@@ -0,0 +1,24 @@
+package org.musicplace.chat.config;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableWebSocket
+@RequiredArgsConstructor
+public class WebSocketConfig implements WebSocketConfigurer {
+    private final WebSocketChatHandler webSocketChatHandler;
+    /**
+     * 웹소켓 연결을 위한 설정
+     * 웹소켓 연결 EndPoint: ws://localhost:8080/chats
+     * 에 연결시 동작할 핸들러는 webSocketChatHandler
+     * @param registry
+     */
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(webSocketChatHandler, "/chats")
+                .addInterceptors(webSocketAuthInterceptor)
+                .setAllowedOrigins("*");
+    }
+}
