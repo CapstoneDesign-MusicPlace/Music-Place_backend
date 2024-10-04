a7cec65418f05c374f0314d8c83cb354c0824ac8
jucheolkang <jucheol9877@naver.com>
2024-10-04 13:39:27 +0900

    refactor : 메소드명 수정

--- src/main/java/org/musicplace/global/exception/ErrorCode.java
+++ src/main/java/org/musicplace/global/exception/ErrorCode.java
@@ -13,7 +13,10 @@ public enum ErrorCode {
     EMAIL_NOT_FOUND("해당 이메일을 찾을 수 없습니다."),
 
     FOLLOW_SAME_ID("동일한 팔로우 ID가 있습니다"),
-    INVALID_CREDENTIALS("x");
+    INVALID_CREDENTIALS("x"),
+
+    SAME_MUSIC("동일한 비디오ID가 있습니다")
+    ;
 
 
     @Getter
--- src/main/java/org/musicplace/playList/repository/MusicRepository.java
+++ src/main/java/org/musicplace/playList/repository/MusicRepository.java
@@ -2,6 +2,10 @@ package org.musicplace.playList.repository;
 
 import org.musicplace.playList.domain.MusicEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface MusicRepository extends JpaRepository<MusicEntity,Long> {
 }
