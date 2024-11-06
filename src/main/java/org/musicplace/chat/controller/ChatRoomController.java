1c1b57751bc1b4f0c2b5396f11c0f454b7db6d73
jucheolkang <jucheol9877@naver.com>
2024-11-06 23:38:18 +0900

    feat : 채팅방 생성 기능 추가

--- src/main/java/org/musicplace/chat/controller/ChatRoomController.java
+++ src/main/java/org/musicplace/chat/controller/ChatRoomController.java
@@ -2,8 +2,10 @@ package org.musicplace.chat.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.musicplace.chat.chatRoom.ChatRoom;
-import org.musicplace.chat.dto.ChatDto;
+import org.musicplace.chat.dto.RoomDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,7 +20,13 @@ public class ChatRoomController {
 
     // 채팅방 목록 조회 엔드포인트
     @GetMapping("/rooms")
-    public Collection<ChatDto> getChatRooms() {
+    public Collection<RoomDto> getChatRooms() {
         return chatRoom.getChatRooms();
+    }
+
+    // 새로운 채팅방 생성 엔드포인트
+    @PostMapping("/rooms")
+    public RoomDto createChatRoom(@RequestBody RoomDto roomDto) {
+        return chatRoom.createChatRoom(roomDto);
     }
 }
