5cc39c8ec6f3873664f85fb53a97320a08efec6e
jucheolkang <jucheol9877@naver.com>
2024-11-11 00:09:49 +0900

    feat : exit 기능 추가

--- src/main/java/org/musicplace/chat/redis/RedisServiceImpl.java
+++ src/main/java/org/musicplace/chat/redis/RedisServiceImpl.java
@@ -2,17 +2,19 @@ package org.musicplace.chat.redis;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.stereotype.Service;
 import org.springframework.web.socket.WebSocketSession;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
 
-import java.util.Objects;
-
-import static org.springframework.boot.availability.AvailabilityChangeEvent.publish;
 
 @Service
 @RequiredArgsConstructor
 public class RedisServiceImpl {
     private final StringRedisTemplate stringRedisTemplate;
+    private final RedisMessageListenerContainer redisMessageListenerContainer;
+
     /**
      * 메시지 발행
      * @param channel 채널
@@ -21,33 +23,35 @@ public class RedisServiceImpl {
     public void publish(String channel, String message) {
         stringRedisTemplate.convertAndSend(channel, message);
     }
+
     /**
      * 메시지 구독
      * @param channel 채널
      * @param session WebSocketSession
      */
     public void subscribe(String channel, WebSocketSession session) {
-        Objects.requireNonNull(stringRedisTemplate.getConnectionFactory())
-                .getConnection()
-                .subscribe(getMessageHandler(session), channel.getBytes());
+        redisMessageListenerContainer.addMessageListener(
+                new MessageListenerAdapter(getMessageHandler(session)),
+                new ChannelTopic(channel)
+        );
     }
+
     /**
-     * 여러 채널 메시지 구독
+     * 메시지 구독 해제
      * @param channel 채널
      * @param session WebSocketSession
      */
-    public void subscribe(String[] channel, WebSocketSession session) {
-        for (String c : channel){
-            Objects.requireNonNull(stringRedisTemplate.getConnectionFactory())
-                    .getConnection()
-                    .subscribe(getMessageHandler(session), c.getBytes());
-        }
+    public void unsubscribe(String channel, WebSocketSession session) {
+        redisMessageListenerContainer.removeMessageListener(
+                new MessageListenerAdapter(getMessageHandler(session)),
+                new ChannelTopic(channel)
+        );
     }
-
 
     /**
      * 메세지 핸들러 생성
      * @param session WebSocketSession
+     * @return RedisMessageHandler
      */
     private RedisMessageHandler getMessageHandler(WebSocketSession session) {
         return new RedisMessageHandler(session);
