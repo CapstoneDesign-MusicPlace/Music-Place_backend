e4eece160bbeb2917643cdf058247b4c27437ca2
jucheolkang <jucheol9877@naver.com>
2024-11-19 23:13:39 +0900

    도커 컴포즈 수정

--- src/main/java/org/musicplace/global/env/DotenvConfig.java
+++ src/main/java/org/musicplace/global/env/DotenvConfig.java
@@ -11,21 +11,14 @@ public class DotenvConfig {
 
     @PostConstruct
     public void loadEnvVariables() {
-        // 첫 번째 .env 파일 로드
-        Dotenv dotenv1 = Dotenv.configure()
-                .directory("/home/a01098774665/")
-                .filename(".env")
-                .load();
 
-        // 두 번째 .env 파일 로드
-        Dotenv dotenv2 = Dotenv.configure()
-                .directory("/home/")
+        Dotenv dotenv = Dotenv.configure()
+                .directory("/data/")
                 .filename(".env")
                 .load();
 
         // 환경 변수에 설정
-        dotenv1.entries().forEach(entry -> setSystemProperty(entry.getKey(), entry.getValue()));
-        dotenv2.entries().forEach(entry -> setSystemProperty(entry.getKey(), entry.getValue()));
+        dotenv.entries().forEach(entry -> setSystemProperty(entry.getKey(), entry.getValue()));
     }
 
     private void setSystemProperty(String key, String value) {
