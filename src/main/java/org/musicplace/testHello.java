fb8458eb0b9e7ddc28ba40c45bd6fa27dbec94af
jucheolkang <jucheol9877@naver.com>
2024-11-19 17:04:10 +0900

    feat : 테스트용 controller 추가

---
+++ src/main/java/org/musicplace/testHello.java
@@ -0,0 +1,17 @@
+package org.musicplace;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/hello")
+public class testHello {
+    @GetMapping()
+    public String getHello() {
+        return "hello world";
+    }
+}
